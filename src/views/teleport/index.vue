d388d38952b74c8437e5cee1ae7306830288b7e9
liujin <2454243839@qq.com>
2023-03-28 10:42:47 +0800

    Transition内置组件

--- src/views/teleport/index.vue
+++ src/views/teleport/index.vue
@@ -12,16 +12,62 @@
     </Teleport>
   </div>
   <div id="father">模拟一个父元素</div>
+
+  <div>====================================================</div>
+  <br />
+
+  <button @click="commonData.show = !commonData.show">Toggle</button>
+  <Transition>
+    <p v-if="commonData.show">hello</p>
+  </Transition>
+
+  <div>====================================================</div>
+  <br />
+
+  <Transition name="bounce">
+    <p v-if="commonData.show" style="text-align: center">
+      Hello here is some bouncy text!
+    </p>
+  </Transition>
 </template>
 <script lang="ts" setup>
 import { reactive, ref, watch, watchEffect, computed } from "vue";
 
 const commonData = reactive({
-  isMobile: false,
+  isMobile: true,
+  show: false,
 });
 </script>
 
 <style scoped>
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
+}
+.bounce-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.25);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+/* 下面我们会解释这些 class 是做什么的 */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
 .trans {
   width: 200px;
   height: 200px;
