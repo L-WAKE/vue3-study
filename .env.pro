30178a32ce2ad0b7344be8c0681cfa5c4676783e
liujin <2454243839@qq.com>
2023-08-17 09:41:46 +0800

    配置打包生产环境剔除console/debugger

--- .env.pro
+++ .env.pro
@@ -1,4 +1,5 @@
-APP_NODE_ENV = "production"
-APP_BASE_API = "http://172.16.61.146:2000/"
-
+# APP_NODE_ENV = "production"
+# APP_BASE_API = "http://172.16.61.146:2000/"
+VITE_NODE_ENV = "production"
+VITE_BASE_API = "http://172.16.61.146:2000/"
 //环境变量必须要以'VITE_'开头，也可在vite.config.ts里面配置 envPrefix: "APP_",
--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,47 @@
-import type { UserConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from "path"
 
-const config: UserConfig = {
-    plugins: [vue()],
-    server: {
-        host: '0.0.0.0',
-        open: false,//启动项目自动弹出浏览器
-        port: 4000,//启动端口
-        proxy: {
-            '/api': {
-                target: 'https://928a6c57-d243-4dce-a4ee-388405267fa5.mock.pstmn.io',	//实际请求地址
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
+export default ({ mode }) => {
+    const env: any = loadEnv(mode, process.cwd());   // 获取.env文件里定义的环境变量-对象类型
+    return defineConfig({
+        plugins: [vue()],
+        build: {
+            minify: 'terser',
+            terserOptions: {
+                compress: {
+                    //生产环境时移除console/debugger
+                    drop_console: env.VITE_NODE_ENV === 'production',
+                    drop_debugger: env.VITE_NODE_ENV === 'production',
+                },
             },
-        }
-    },
-    resolve: {
-        // ↓路径别名
-        alias: {
-            "@": resolve(__dirname, "./src")
-        }
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `@import "@/style/mixin.scss";`
+        },
+        server: {
+            host: '0.0.0.0',
+            open: false,//启动项目自动弹出浏览器
+            port: 4000,//启动端口
+            proxy: {
+                '/api': {
+                    target: 'https://928a6c57-d243-4dce-a4ee-388405267fa5.mock.pstmn.io',	//实际请求地址
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, '')
+                },
             }
-        }
-    },
-    envPrefix: "APP_",
+        },
+        resolve: {
+            // ↓路径别名
+            alias: {
+                "@": resolve(__dirname, "./src")
+            },
+            dedupe: ['vue']
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@import "@/style/mixin.scss";`
+                }
+            }
+        },
+        // envPrefix: "APP_",
+    })
 }
-export default config
